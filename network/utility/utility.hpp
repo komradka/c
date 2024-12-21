64ca1d8461b5420df6baf9bc97218e668088c222
Nikita Proshchaev <langenik888@gmail.com>
2024-12-21 21:35:36 +0300

    NEW: nd: Range based for loop.

--- network/utility/utility.hpp
+++ network/utility/utility.hpp
@@ -42,3 +42,44 @@ std::string get_name(int e);
 std::string get_name(double e);
 
 std::string get_name(bool e);
+
+template<typename Int>
+class counting_iterator_t
+{
+private:
+  Int m_value;
+
+public:
+  counting_iterator_t (Int value) : m_value (value) {}
+
+  Int operator* () { return m_value; }
+  counting_iterator_t operator++ () { return counting_iterator_t{m_value++}; }
+
+  friend bool operator== (const counting_iterator_t &lhs, const counting_iterator_t &rhs) { return lhs.m_value == rhs.m_value; }
+  friend bool operator!= (const counting_iterator_t &lhs, const counting_iterator_t &rhs) { return lhs.m_value != rhs.m_value; }
+  friend bool operator< (const counting_iterator_t &lhs, const counting_iterator_t &rhs) { return lhs.m_value < rhs.m_value; }
+  friend bool operator> (const counting_iterator_t &lhs, const counting_iterator_t &rhs) { return lhs.m_value > rhs.m_value; }
+  friend bool operator<= (const counting_iterator_t &lhs, const counting_iterator_t &rhs) { return lhs.m_value <= rhs.m_value; }
+  friend bool operator>= (const counting_iterator_t &lhs, const counting_iterator_t &rhs) { return lhs.m_value >= rhs.m_value; }
+};
+
+template<typename Int>
+class counting_range_t
+{
+private:
+  Int m_begin, m_end;
+
+public:
+  using iterator = counting_iterator_t<Int>;
+
+  counting_range_t (Int begin, Int end) : m_begin (begin), m_end (end) {}
+
+  iterator begin () const { return iterator{m_begin}; }
+  iterator end () const { return iterator{m_end}; }
+};
+
+template<typename Int>
+counting_range_t<Int> range (Int begin, Int end)
+{
+  return {begin, end};
+}
