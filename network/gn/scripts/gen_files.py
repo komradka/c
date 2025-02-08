36de3f73d5c073e0b708e5e44446b79237206568
Nikita Proshchaev <langenik888@gmail.com>
2025-02-08 16:38:44 +0300

    FIX: build: Partial fix rebuilding moc files.

--- network/gn/scripts/gen_files.py
+++ network/gn/scripts/gen_files.py
@@ -3,17 +3,26 @@ import argparse
 import subprocess
 import sys
 
+def update_file (content, filename):
+    file = open (filename, "a+")
+    file.seek (0)
+    if content != file.read ():
+        file.truncate (0)
+        file.write (content)
+    file.close ()
+
 def make_gen_dirs (path):
-    for dir_name in path:
-        directory = os.path.dirname (dir_name)
+        directory = os.path.dirname (path)
         if not os.path.exists (directory):
             os.makedirs (directory)
 
+def get_tmp_output (output):
+    return f'{output}_tmp'
 
 def main ():
     parser = argparse.ArgumentParser ()
     parser.add_argument ('--rule', dest='rule', required=True, nargs=1)
-    parser.add_argument ('--output', dest='output', required=True, nargs=1)
+    parser.add_argument ('--output', dest='output', required=True)
 
     args, unknownargs = parser.parse_known_args ()
 
@@ -24,10 +33,16 @@ def main ():
 
     make_gen_dirs (args.output)
 
-    exit_code = subprocess.call (rule, stdin=None, stderr=None)
+    tmp_output = get_tmp_output (args.output)
+    exit_code = subprocess.call (rule + ["-o", tmp_output], stdin=None, stderr=None)
     if exit_code != 0:
       sys.exit (exit_code)
 
+    tmp_file = open (tmp_output, "r")
+    update_file (tmp_file.read (), args.output)
+
+    tmp_file.close ()
+    os.remove (tmp_output)
 
 if __name__ == '__main__':
     main ()
